4ab42e106cab62a10a67f9f107a2fc8fbea81b02
Nick Cheng <nicholascheng25@gmail.com>
2021-04-08 00:20:26 +1000

    opening new chromes and going to page which shows isp provider

--- sniffer.py
+++ sniffer.py
@@ -1,9 +1,21 @@
 #! /usr/bin/env python
 from scapy.all import *
 conf.use_pcap = True
+from selenium import webdriver
+import os
+#post-5236 > div.container-fluid > div:nth-child(1) > form > div > input
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+DRIVER_BIN = os.path.join(PROJECT_ROOT, "chromedriver")
+DRIVER_PATH = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 
 def print_packet(packet):
     ip_layer = packet.getlayer(IP)
-    print("[!] New Packet: {src} -> {dst}".format(src=ip_layer.src, dst=ip_layer.dst))
+    src = ip_layer.src
+    driver = webdriver.Chrome(executable_path = DRIVER_BIN)
+    driver.get('https://www.whatismyip.com/' + src + '/')
+    print("[!] New Packet:")
+
+    
 
 a=sniff(iface = "en0", filter = "ip", prn=print_packet)
+
