189beaac48922bfe0a7cc28e8046fd364171a636
Nick Cheng <nicholascheng25@gmail.com>
2021-04-11 18:23:59 +1000

    prints first host'

--- sniffer.py
+++ sniffer.py
@@ -2,20 +2,27 @@
 from scapy.all import *
 conf.use_pcap = True
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 import os
 #post-5236 > div.container-fluid > div:nth-child(1) > form > div > input
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 DRIVER_BIN = os.path.join(PROJECT_ROOT, "chromedriver")
 DRIVER_PATH = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-
+driver = webdriver.Chrome(executable_path = DRIVER_BIN)
 def print_packet(packet):
     ip_layer = packet.getlayer(IP)
     src = ip_layer.src
-    driver = webdriver.Chrome(executable_path = DRIVER_BIN)
-    driver.get('https://www.whatismyip.com/' + src + '/')
-    print("[!] New Packet:")
+    if '192.168' not in src:
+        driver.get('https://www.whatismyip.com/' + src + '/') 
+        driver.execute_script("window.scrollTo(0, 30)") 
+        try:
+            driver.find_elements_by_xpath("//*[contains(text(), 'ISP:')]").click()
+        except:
+            print('continue')
 
-    
-
+        el = driver.find_element_by_xpath("//*[contains(text(), 'ISP:')]")
+        print("[!] Host: ", end = '')
+        print(el.text)
+       
 a=sniff(iface = "en0", filter = "ip", prn=print_packet)
 
